bd05c7bcafef2c38ff179e5207e61d63dfa7fce3
Ashish Raste <rasteashish@gmail.com>
2013-10-04 16:24:59 +0800

    T1: top5 movies avg ratings

--- wa2.py
+++ wa2.py
@@ -6,7 +6,7 @@ import math as math
 import numpy as np
 
 
-location = r'/home/freax/Downloads/recsys_data_WA 1 Rating Matrix.csv'
+location = r'/home/freax/recommender-systems/recsys_data_WA 1 Rating Matrix.csv'
 df = pd.read_csv(location, header=None)
 df.columns = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 nb = np.zeros(20)
