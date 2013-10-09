c1d5db35b74ffcf4798a1f75e043c6d0f43a3085
Ashish Raste <rasteashish@gmail.com>
2013-10-09 13:41:32 +0800

    intro: lenskit-hello with delimiter issues

---
+++ programming-assignment1/pa1/src/test/java/org/freax/recsys/pa1/AppTest.java
@@ -0,0 +1,22 @@
+package org.freax.recsys.pa1;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertThat(true, equalTo(true));
+    }
+}
+
